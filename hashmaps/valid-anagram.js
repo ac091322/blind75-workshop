3da0653a63107514a4285df7a1115941f83cdf80
alan-chang <alanc091322@gmail.com>
2024-11-15 19:46:08 -0800

    updated

--- hashmaps/valid-anagram.js
+++ hashmaps/valid-anagram.js
@@ -80,7 +80,7 @@ const isAnagram = (s, t) => {
 }
 
 
-// console.log(isAnagram("racecar", "carrace"));  // output: true
+console.log(isAnagram("racecar", "carrace"));  // output: true
 // console.log(isAnagram("test", "testtest"));  // output: false
-console.log(isAnagram("testtast", "testtest"));  // output: false
+// console.log(isAnagram("testtast", "testtest"));  // output: false
 // console.log(isAnagram("jar", "jam"));  // output: false
