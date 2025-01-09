b6df8be47201ad3a0f82a58cb252b887c344b072
Alan Chang <alanc091322@gmail.com>
2025-01-09 01:31:34 -0800

    updated

--- sliding-window/longest-repeating-character-replacement.py
+++ sliding-window/longest-repeating-character-replacement.py
@@ -99,7 +99,7 @@ def character_replacement(s: str, k: int) -> int:
         # subtract the max frequency of any character count from the window size to get the number of non-target characters (the characters that are not the most frequent)
         while (right_pointer - left_pointer + 1) - max_char_freq > k:
             left_char = s[left_pointer]
-            # still need to decrease the count of the character in case another target character is found, which will increase the count by 1 again
+            # still need to decrease the count of the character in case another target character is found, which will increase the count by 1 again (decreasing the count is not used for breaking out of the while loop)
             # it doesn't matter if the character at the left pointer is the target character, it is the character that is existing the window, so it must be decreased
             char_counter[left_char] -= 1
             left_pointer += 1
