0889b56393f0dae09651f2bc806b853bc42eb1c2
Alan Chang <alanc091322@gmail.com>
2025-01-15 17:43:18 -0800

    added Scratch.java file

---
+++ Scratch.java
@@ -0,0 +1,19 @@
+public class Scratch {
+
+    public static void main(String[] args) {
+        int first = 5;
+        int second = 10;
+
+        beginningToMiddle(first, second);
+
+        System.out.println(first);
+    }
+
+    public static void beginningToMiddle (int start, int end) {
+        int middle = (start + end)/2;
+        while (start < middle) {
+            System.out.println("step");
+            start++;
+        }
+    }
+}
